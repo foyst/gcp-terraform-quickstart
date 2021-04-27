909a17b58d0c63476d1a58547773b416be0e8e6c
Ben Foster <ben.foster@infinityworks.com>
2021-04-27 22:05:16 +0100

    Automating provisioning of Docker and nginx container

--- main.tf
+++ main.tf
@@ -45,9 +45,16 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "centos-7-v20210420"
     }
   }
+
+  metadata_startup_script = <<EOT
+curl -fsSL https://get.docker.com -o get-docker.sh && 
+sudo sh get-docker.sh && 
+sudo service docker start && 
+docker run -p 8080:80 -d nginxdemos/hello
+EOT
 
   network_interface {
     network = google_compute_network.vpc_network.self_link
