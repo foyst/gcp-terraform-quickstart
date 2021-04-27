fb8467bd351cb851922dfd62c5260b7c973f4e4f
Ben Foster <ben.foster@infinityworks.com>
2021-04-27 22:07:01 +0100

    Removing SSH connectivity and adding NAT Gateway

--- main.tf
+++ main.tf
@@ -29,6 +29,19 @@ resource "google_compute_subnetwork" "private_network" {
   network       = google_compute_network.vpc_network.self_link
 }
 
+resource "google_compute_router" "router" {
+  name    = "quickstart-router"
+  network = google_compute_network.vpc_network.self_link
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "quickstart-router-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
 resource "google_compute_route" "private_network_internet_route" {
   name             = "private-network-internet"
   dest_range       = "0.0.0.0/0"
@@ -58,11 +71,11 @@ EOT
 
   network_interface {
     network = google_compute_network.vpc_network.self_link
-    subnetwork = google_compute_subnetwork.private_network.self_link
-
-    access_config {
-      network_tier = "STANDARD"
-    }
+    subnetwork = google_compute_subnetwork.private_network.self_link    
+    # access_config {
+    #   network_tier = "STANDARD"
+    # }
+  }
   }
 }
 
@@ -70,10 +83,19 @@ resource "google_compute_firewall" "public_ssh" {
   name    = "public-ssh"
   network = google_compute_network.vpc_network.self_link
 
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
+# resource "google_compute_firewall" "public_ssh" {
+#   name    = "public-ssh"
+#   network = google_compute_network.vpc_network.self_link
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["22"]
+#   }
+
+#   direction = "INGRESS"
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags = ["nginx-instance"]
+# }
 
   direction = "INGRESS"
   source_ranges = ["0.0.0.0/0"]
